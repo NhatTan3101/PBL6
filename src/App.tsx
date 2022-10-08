5ab7b238223eb926d4f220ed0f459758f44a0ba3
Hoang Nhat Tan <hoangnhattan31012001@gmail.com>
2022-10-08 20:09:11 +0700

    Header Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Header />
     </div>
   );
 }
